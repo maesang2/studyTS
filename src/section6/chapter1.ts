8a883ec12410a0be67faa83837c3b16205f20318
LHG <i2376@naver.com>
2024-08-20 23:52:29 +0900

    TS-09: 타입스크립트의 클래스

---
+++ src/section6/chapter1.ts
@@ -0,0 +1,47 @@
+/**
+ * 타입 스크립트의 클래스
+ */
+
+const employee = {
+    name: "이정환",
+    age: 27,
+    position: "developer",
+    work() {
+        console.log("일침");
+    },
+};
+
+class Employee {
+    // 필드
+    name: string;
+    age: number;
+    position: string;
+
+// 생성자
+    constructor(name: string, age: number, position: string) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
+
+    // 메서드
+    work() {
+        console.log("일함")
+    }
+}
+
+class ExecutiveOfficer extends Employee {
+    // 필드
+    officeNumber: number;
+
+    // 생성자
+    constructor(name: string,
+                age: number,
+                position: string,
+                officeNumber: number) {
+        super(name, age, position);
+        this.officeNumber = officeNumber;
+    }
+}
+
+const employeeB = new Employee("이정환", 27, "개발자");
